2befefa5214ee7cfeeecea288aa794f0686d1202
ravi406 <ravikamble406@gmail.com>
2022-11-24 06:51:58 +0530

    Added program to swap Two Numbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+	
+	 public static void main(String[] args) {
+
+	        int num1, num2;
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Before swapping");
+	        System.out.println("Enter a First number :");
+	        num1 = sc.nextInt();
+	        System.out.println("Enter a second number :");
+	        num2 = sc.nextInt();
+
+	        int temp = num1;
+	        num1 = num2;
+	        num2 = temp;
+
+	        System.out.println("After swapping");
+	        System.out.println("The First number is:" + num1);
+	        System.out.println("The second number is :" + num2);
+	    }
+}
